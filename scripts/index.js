cae729dd7dece5e55d3027a2e6202a9ba3d9dbed
Andrew Mora <ajmora7@gmail.com>
2018-03-08 20:36:55 -0800

    Added navbar w/ javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,18 @@
-var hello = 'Hello world!';
-
-console.log(hello);
-
 var navItem = document.querySelector('.navbar');
 console.log(navItem);
+
+window.onscroll = function() {
+    floatNavBar();
+}
+
+var navBar = document.querySelector('.navbar');
+
+var sticky = navBar.offsetTop;
+
+function floatNavBar() {
+    if (window.pageYOffset >= sticky) {
+        navBar.classList.add('sticky');
+    } else {
+        navBar.classList.remove('sticky');
+    }
+}
